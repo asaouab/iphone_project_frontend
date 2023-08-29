cbeae646ebdb86306fa9f3ff76a15756f8ce4a66
Adam Saouab <asaouab@e3r9p16.1337.ma>
2023-08-29 16:32:46 +0100

    adam

---
+++ code.js
@@ -0,0 +1,10 @@
+let picture = document.querySelector('.mid');
+let container = document.querySelector('.all')
+function change(path)
+{
+    picture.src = path;
+}
+function color(para)
+{
+    container.style.background = para;
+}
